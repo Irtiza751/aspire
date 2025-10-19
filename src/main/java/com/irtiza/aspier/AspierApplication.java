869cea7cfd8cf1876c4f7356907ed1c12baf412c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-10-19 20:44:12 +0500

    initial commit

---
+++ src/main/java/com/irtiza/aspier/AspierApplication.java
@@ -0,0 +1,13 @@
+package com.irtiza.aspier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irtiza/aspier/AspierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irtiza.aspier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
