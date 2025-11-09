bd9a0928bde68b0803b33ed5bcd18132a3815742
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-11-09 14:19:20 +0500

    implement ProductImage & ProductSize entities and add relations

---
+++ src/main/java/com/irtiza/aspier/entity/ProductImage.java
@@ -0,0 +1,48 @@
+package com.irtiza.aspier.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_images")
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public ProductImage() {}
+
+    public ProductImage(String url, Product product) {
+        this.url = url;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
